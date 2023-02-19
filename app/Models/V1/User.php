037cab971e380606c8dbb6ce641079a18cc45372
Ahmed Essam <aahmedessam30@gmail.com>
2023-02-19 22:33:00 +0200

    Change Directory

--- app/Models/V1/User.php
+++ app/Models/V1/User.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\V1;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
