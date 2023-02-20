596908641d6639190bcc961486065a4482ac07a3
Ahmed Essam <aahmedessam30@gmail.com>
2023-02-21 00:49:02 +0200

    Add Translation

---
+++ lang/en/messages.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'success' => [
+        'create' => 'Successfully created :attribute',
+        'update' => 'Successfully updated :attribute',
+        'delete' => 'Successfully deleted :attribute',
+    ],
+
+    'failed' => [
+        'create' => 'Failed to create :attribute',
+        'update' => 'Failed to update :attribute',
+        'delete' => 'Failed to delete :attribute',
+    ],
+
+    'not_found'      => 'No :attribute found',
+    'error_occurred' => 'An error occurred',
+];
