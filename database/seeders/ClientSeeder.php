8cd92b5568da6eef4a0501def072296f4479d4bb
Ahmed Essam <aahmedessam30@gmail.com>
2023-02-21 00:46:34 +0200

    Update model and controllers

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -19,10 +19,26 @@ class ClientSeeder extends Seeder
                 'name'              => 'Client',
                 'email'             => 'client@algypt.com',
                 'password'          => Hash::make('client'),
+                'address'           => 'Egypt',
                 'email_verified_at' => now(),
-                'remember_token'    => Str::random(10),
             ]);
 
         $client->assignRole('client');
+        $client->media()->create([
+            'name' => 'avatar',
+            'path'  => 'https://ui-avatars.com/api/?name=Client&background=0D8ABC&color=fff',
+            'extension' => 'png',
+            'mime_type' => 'image/png',
+            'size' => '100',
+            'alt' => 'Client',
+            'order' => '1',
+        ]);
+        $client->phone()->create([
+            'phone' => '01000000000',
+            'type' => 'mobile',
+            'is_primary' => '1',
+            'is_active' => '1',
+
+        ]);
     }
 }
