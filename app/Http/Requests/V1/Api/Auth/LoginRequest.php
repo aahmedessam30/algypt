b7bda550e5589622673225ab3448608489ffb763
Ahmed Essam <aahmedessam30@gmail.com>
2023-03-04 23:55:11 +0200

    Fix Unique

---
+++ app/Http/Requests/V1/Api/Auth/LoginRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\V1\Api\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'email'    => ['required', 'string', 'email', 'exists:clients,email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required'    => __('validation.required', ['attribute' => __('validation.attributes.email')]),
+            'email.string'      => __('validation.string', ['attribute' => __('validation.attributes.email')]),
+            'email.email'       => __('validation.email', ['attribute' => __('validation.attributes.email')]),
+            'email.exists'      => __('validation.exists', ['attribute' => __('validation.attributes.email')]),
+            'password.required' => __('validation.required', ['attribute' => __('validation.attributes.password')]),
+            'password.string'   => __('validation.string', ['attribute' => __('validation.attributes.password')]),
+        ];
+    }
+}
