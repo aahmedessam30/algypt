940659a5925f2d7b3c762262ee3ae47788fac042
Ahmed Essam <aahmedessam30@gmail.com>
2023-03-04 23:36:08 +0200

    Update PermissionSeeder.php

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -21,9 +21,12 @@ class PermissionSeeder extends Seeder
                     ['auth', 'sanctum', 'password', 'verification', 'login', 'register', 'logout', 'ignition', 'verify-email', 'resend-verification-email', 'reset-password']);
             })
             ->map(function ($route) {
-                $prefix = Route::getRoutes()->getByName($route)->getAction('prefix');
-                $prefix = str_contains($prefix, '/') ? explode('/', $prefix)[0] : $prefix;
-                return str_replace(array("$prefix.", '.'), array('', '_'), $route) . ($prefix === 'admin' ? '_admin' : '_client');
+                $prefix     = Route::getRoutes()->getByName($route)->getAction('prefix');
+                $prefix     = str_contains($prefix, '/') ? explode('/', $prefix)[0] : $prefix;
+                $permission = in_array($prefix, ['admin', 'api'])
+                    ? str_replace(["$prefix.", '.'], ['', '_'], $route)
+                    : str_replace('.', '_', $route);
+                return $permission . ($prefix === 'admin' ? '_admin' : '_client');
             })
             ->each(function ($route) {
                 Permission::create(['name' => implode('_', array_slice(explode('_', $route), 0, -1)), 'guard_name' => last(explode('_', $route))]);
